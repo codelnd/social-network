f0eedf7514074cfaab95f043dfe0620dc0ca271a
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-19 23:49:28 +0700

    feat: создать базовую структуру проекта

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+const App = () => {
+    return (
+        <div>
+            <header>
+                <img src=''/>
+            </header>
+            <nav>
+                <ul>
+                    <li>Профиль</li>
+                    <li>Сообщения</li>
+                    <li>Новости</li>
+                    <li>Музыка</li>
+                    <li>Настройки</li>
+                </ul>
+            </nav>
+            <main></main>
+        </div>
+    );
+}
+
+export default App;
