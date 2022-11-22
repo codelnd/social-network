8f49e80d685c4fbd088be03f5ebe4ba40af8a52a
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-22 21:01:35 +0700

    feat: создать компонент post

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+            <div>
+                <img src="" alt="image"/>
+                Пост
+            </div>
+    );
+};
+
+export default Post;
