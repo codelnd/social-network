96d51863b55d96cf2de8d2a456b594c27096af9c
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-22 20:59:57 +0700

    refactor: заменить содержимое компонента myposts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,8 +4,13 @@ const MyPosts = () => {
     return (
         <div>
             Посты
+            <div>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <button>Добавить пост</button>
+            </div>
             <div>Новый пост</div>
             <div>
+                <img src="" alt="image"/>
                 Пост
             </div>
         </div>
