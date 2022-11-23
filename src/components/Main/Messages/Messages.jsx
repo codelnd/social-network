2ef3c45fd9398f0fa9a77a0a8fc4b0cf169e768b
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-23 23:26:43 +0700

    feat: стилизовать компоненты

--- src/components/Main/Messages/Messages.jsx
+++ src/components/Main/Messages/Messages.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import s from './Messages.module.css'
 
 const Messages = () => {
     return (
-        <div>
-            Сообщения
-        </div>
+        <section className={s.dialogs}>
+            <div className={s.dialogs__items}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Имя 1
+                </div>
+                <div className={s.dialog}>
+                    Имя 2
+                </div>
+                <div className={s.dialog}>
+                    Имя 3
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Сообщение 1</div>
+                <div className={s.message}>Сообщение 2</div>
+                <div className={s.message}>Сообщение 3</div>
+            </div>
+        </section>
     );
 };
 
