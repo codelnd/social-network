5bf83e8540e1098cdce07aadf512fe7f88c8c9e2
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-24 21:34:20 +0700

    feat: вынести в пропсы id и name

--- src/components/Main/Messages/Name/Name.jsx
+++ src/components/Main/Messages/Name/Name.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import s from "../Messages.module.css";
+import s from "./Name.module.css";
+import {NavLink} from "react-router-dom";
 
 const Name = ({id, name}) => {
     return (
         <div className={`${s.name} ${s.active}`}>
-
+            <NavLink to={`/messages/${id}`}>{name}</NavLink>
         </div>
     );
 };
