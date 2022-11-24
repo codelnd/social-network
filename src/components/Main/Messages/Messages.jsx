1b95506a9ede043c0ccef4164749b75efeb67f9e
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-24 21:37:54 +0700

    feat: вывести имена через атрибуты компонента name

--- src/components/Main/Messages/Messages.jsx
+++ src/components/Main/Messages/Messages.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import s from './Messages.module.css'
 import {NavLink} from "react-router-dom";
+import Name from "./Name/Name";
 
 const Messages = () => {
     return (
         <section className={s.messages}>
             <div className={s.names}>
                 <div className={`${s.name} ${s.active}`}>
-                   <NavLink to='/messages/1'>Денис</NavLink>
-                </div>
-                <div className={s.name}>
-                    <NavLink to='/messages/2'>Кристина</NavLink>
-                </div>
-                <div className={s.name}>
-                    <NavLink to='/messages/3'>Полина</NavLink>
+                    <Name id='1' name='Денис'/>
+                    <Name id='2' name='Кристина'/>
+                    <Name id='3' name='Полина'/>
                 </div>
             </div>
             <div className={s.messages__items}>
