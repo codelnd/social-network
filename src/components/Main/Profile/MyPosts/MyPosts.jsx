f4b8c09589d2000b0325fb0a2422566e09fbeb8a
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-28 21:01:32 +0700

    feat: создать модуль стилей для myposts

--- src/components/Main/Profile/MyPosts/MyPosts.jsx
+++ src/components/Main/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Post from "./Post/Post";
+import s from "./MyPosts.module.css"
 
 const MyPosts = () => {
     return (
         <div>
             Посты
             <div>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea name="" id="" cols="20" rows="4"></textarea>
                 <button>Добавить пост</button>
             </div>
-            {/*<div>Новый пост</div>*/}
             <Post message={'Привет'}/>
             <Post message={'Пост №1'}/>
         </div>
