30e3d1ca56c6042918f1120c181d9d97f148500e
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-10 23:22:37 +0700

    feat: создать объект с данными в стейте

--- src/index.js
+++ src/index.js
@@ -2,63 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import {BrowserRouter} from "react-router-dom";
+import state from "./state/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const nameData = [
-    {
-        id: 1,
-        name: 'Денис'
-    },
-    {
-        id: 2,
-        name: 'Ваня'
-    }, {
-        id: 3,
-        name: 'Виталя'
-    }, {
-        id: 4,
-        name: 'Полина'
-    },
-]
-const messageData = [
-    {
-        id: 1,
-        message: 'Hi, bro'
-    },
-    {
-        id: 2,
-        message: 'Ok'
-    },
-    {
-        id: 3,
-        message: 'What is it?'
-    },
-    {
-        id: 4,
-        message: 'How are you?'
-    },
-]
-const postData = [
-    {
-        id: 1,
-        message: 'Всем привет',
-        likes: 10
-    },
-    {
-        id: 2,
-        message: 'Мой первый пост',
-        likes: 4
-    },
-    {
-        id: 3,
-        message: 'Освоил redux!',
-        likes: 99
-    },
-]
-
 root.render(
     <BrowserRouter>
-        <App nameData={nameData} messageData={messageData} postData={postData}/>
+        <App nameData={state.nameData} messageData={state.messageData} postData={state.postData}/>
     </BrowserRouter>
 );
