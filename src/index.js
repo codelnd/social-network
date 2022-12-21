263cb54141fdeef3737dca257519e97d83f1517d
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-21 21:51:10 +0700

    feat: передать в subscribe rerender

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import state, {addPost, updatePostMessage} from "./state/state";
+import state, {addPost, subscribe, updatePostMessage} from "./state/state";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import App from "./components/App/App";
@@ -18,3 +18,5 @@ const rerender = () => {
 }
 
 rerender(state)
+
+subscribe(rerender)
