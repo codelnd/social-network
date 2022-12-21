41cbb94a92347e40654874e425caaf9aa176ca1b
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-21 18:27:20 +0700

    fix: удалить render.js, исправить архитектурную ошибку

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {rerender} from './render'
-import state from "./state/state";
+import state, {addPost, updatePostMessage} from "./state/state";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+import App from "./components/App/App";
 
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export const rerender = (state) => {
+    root.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updatePostMessage={updatePostMessage}/>
+        </BrowserRouter>
+    );
+}
 
 rerender(state)
