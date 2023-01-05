16bb0fca62654936ddbbff1de5cc79a11ac157e2
Denis Bezukladnikov <denis3094@mail.ru>
2023-01-05 21:07:06 +0700

    refactor: закомментить неиспользуемый код

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,86 +1,86 @@
-import profileReducer from "./profile-reducer";
-import messagesReducer from "./messages-reducer";
-
-
-const store = {
-    _state: {
-        messagesPage: {
-            nameData: [
-                {
-                    id: 1,
-                    name: 'Денис'
-                },
-                {
-                    id: 2,
-                    name: 'Ваня'
-                }, {
-                    id: 3,
-                    name: 'Виталя'
-                }, {
-                    id: 4,
-                    name: 'Полина'
-                },
-            ],
-            messageData: [
-                {
-                    id: 1,
-                    message: 'Hi, bro'
-                },
-                {
-                    id: 2,
-                    message: 'Ok'
-                },
-                {
-                    id: 3,
-                    message: 'What is it?'
-                },
-                {
-                    id: 4,
-                    message: 'How are you?'
-                },
-            ],
-            newMessage: '',
-        },
-        profilePage: {
-            postData: [
-                {
-                    id: 1,
-                    message: 'Всем привет',
-                    likes: 10
-                },
-                {
-                    id: 2,
-                    message: 'Мой первый пост',
-                    likes: 4
-                },
-                {
-                    id: 3,
-                    message: 'Освоил redux!',
-                    likes: 99
-                },
-            ],
-            newPostMessage: '',
-        },
-    },
-
-    _callSubscriber() {
-        console.log('no subscribers')
-    },
-
-    getState() {
-        return this._state
-    },
-
-    subscribe(observer) {
-        this._callSubscriber = observer
-    },
-
-    dispatch(action) {
-        this._state.profilePage = profileReducer(this._state.profilePage, action)
-        this._state.messagesPage = messagesReducer(this._state.messagesPage, action)
-        this._callSubscriber(this._state)
-    },
-}
-
-
-export default store
+// import profileReducer from "./profile-reducer";
+// import messagesReducer from "./messages-reducer";
+//
+//
+// const store = {
+//     _state: {
+//         messagesPage: {
+//             nameData: [
+//                 {
+//                     id: 1,
+//                     name: 'Денис'
+//                 },
+//                 {
+//                     id: 2,
+//                     name: 'Ваня'
+//                 }, {
+//                     id: 3,
+//                     name: 'Виталя'
+//                 }, {
+//                     id: 4,
+//                     name: 'Полина'
+//                 },
+//             ],
+//             messageData: [
+//                 {
+//                     id: 1,
+//                     message: 'Hi, bro'
+//                 },
+//                 {
+//                     id: 2,
+//                     message: 'Ok'
+//                 },
+//                 {
+//                     id: 3,
+//                     message: 'What is it?'
+//                 },
+//                 {
+//                     id: 4,
+//                     message: 'How are you?'
+//                 },
+//             ],
+//             newMessage: '',
+//         },
+//         profilePage: {
+//             postData: [
+//                 {
+//                     id: 1,
+//                     message: 'Всем привет',
+//                     likes: 10
+//                 },
+//                 {
+//                     id: 2,
+//                     message: 'Мой первый пост',
+//                     likes: 4
+//                 },
+//                 {
+//                     id: 3,
+//                     message: 'Освоил redux!',
+//                     likes: 99
+//                 },
+//             ],
+//             newPostMessage: '',
+//         },
+//     },
+//
+//     _callSubscriber() {
+//         console.log('no subscribers')
+//     },
+//
+//     getState() {
+//         return this._state
+//     },
+//
+//     subscribe(observer) {
+//         this._callSubscriber = observer
+//     },
+//
+//     dispatch(action) {
+//         this._state.profilePage = profileReducer(this._state.profilePage, action)
+//         this._state.messagesPage = messagesReducer(this._state.messagesPage, action)
+//         this._callSubscriber(this._state)
+//     },
+// }
+//
+//
+// export default store
