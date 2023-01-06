477a04d20638453a7aa9a38ab44b9f810be1caab
Denis Bezukladnikov <denis3094@mail.ru>
2023-01-06 19:50:00 +0700

    feat: создать root reducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,11 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import messagesReducer from "./messages-reducer";
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer
 })
 
-const store = createStore(reducers)
+const store = createStore(rootReducer)
 
 export default store
