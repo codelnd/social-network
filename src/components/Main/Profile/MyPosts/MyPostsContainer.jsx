f2998172a54be1cd56b683c404f8ee5c36e95aad
Denis Bezukladnikov <denis3094@mail.ru>
2023-01-07 23:48:10 +0700

    refactor: удалить лишние импорты из контейнерной компоненты

--- src/components/Main/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Main/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Post from "./Post/Post";
-import s from "./MyPosts.module.css"
 import {addPostCreator, updatePostCreator} from "../../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 
