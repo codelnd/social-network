081323f3da6ea31e6cee466dbf6fcdd1e6f1f770
Denis Bezukladnikov <denis3094@mail.ru>
2023-01-09 22:10:40 +0700

    feat: обернуть app в провайдер

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import App from "./components/App/App";
 import store from "./redux/redux-store";
+import MyContext from "./MyContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const rerender = (state) => {
     root.render(
         <BrowserRouter>
-            <App store={store}/>
+            <MyContext.Provider value={store}>
+                <App store={store}/>
+            </MyContext.Provider>
         </BrowserRouter>
     );
 }
